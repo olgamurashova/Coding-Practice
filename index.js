e348746903f33e8113bc7b46669492f63d3e27c9
Olga Murashova <om.dreyfus@gmail.com>
2022-06-19 20:02:53 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-ReactDOM.render(document.getElementById("root"))
+// ReactDOM.render(document.getElementById("root"))
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is the way to go"
+h1.className = "header"
+document.getElementById("root").append(h1)
